7d5fdd6ddfd5674befbdc8597bd6e57f9facaedc
Hadson Palauro Sampaio <sampaiohadson1704@outlook.com>
2022-04-21 01:30:11 -0300

    Criar home

--- app/src/main/java/com/example/brcommobileimovelparatodos/MainActivity.kt
+++ app/src/main/java/com/example/brcommobileimovelparatodos/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.brcommobileimovelparatodos
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+    import android.os.Bundle
+    import android.widget.*
+    import androidx.core.content.ContextCompat
+    import androidx.core.view.ViewCompat
+
+    class MainActivity : AppCompatActivity() {
+        private val context: Context get() = this
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val acessarMin = findViewById<TextView>(R.id.acessar_min)
+        acessarMin.setOnClickListener{
+            val intent = Intent(context, Login::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
